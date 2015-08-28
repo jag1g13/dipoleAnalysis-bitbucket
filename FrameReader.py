f3abf66c883808a505239291c95e12ec283e991e
marley95 <marleysamways@users.noreply.github.com>
2015-08-28 14:38:42 +0100

    First commit

---
+++ FrameReader.py
@@ -0,0 +1,162 @@
+
+class FrameReader(object):
+    def __init__(self, filename):
+        file_ = open(filename, 'r')
+        print("Read file: {0}".format(filename))
+        self.all_lines = file_.readlines()
+        file_.close()
+        self.total_frames = getNumFrames(self)
+        self.total_atoms = getNumAtoms(self)
+        # self.atom_lines = extractAtomLines_all(self)
+
+    def getNumFrames(self):
+        """
+        Get the number of frames present in the open trajectory
+        :return: Number of frames in trajectory
+        """
+        timestep_values = readTimesteps(self.all_lines)    # Takes the values of all timesteps
+        return len(timestep_values)
+
+    def getNumAtoms(self):
+        """
+        Get the number of atoms present in the open trajectory
+        :return: Number of atoms in trajectory
+        """
+        count = 0
+        for line in self.all_lines:
+            if line[:21] == "ITEM: NUMBER OF ATOMS":
+                numOfAtoms = int(self.all_lines[count+1])
+                break
+            else:
+                count += 1
+        return numOfAtoms
+
+    def readTimesteps(self):
+        """ Function to read the timesteps from the input
+        trajectory, so that these can be written into the
+        output file.
+        lammpstrj - the raw, unedited trajectory. """
+        count = 0       # Used to keep track of the lines
+        timesteps = []  # Used to store the timestep values
+        for line in self.all_lines:
+            if line[:14] == "ITEM: TIMESTEP":
+                timesteps.append(self.all_lines[count + 1])   # Line after the declaration contains the timestep
+                count += 1
+                continue
+            else:
+                count += 1
+                continue
+        return timesteps    # Length of this list will be equal to the number of frames
+
+    def readAtomCoords(lammpstrj, raw_atom_lines):
+        """ Function to obtain the x,y,z coordinates for
+        a particular atom at each frame in the trajectory.
+        lammpstrj - raw, unedited lammps trajectory.
+        raw_atom_lines - raw, unedited lines, specific to
+          one atom. """
+        # Function works in the same way as readAtomDipoles(), but written as separate functions for clarity
+        for line in lammpstrj:
+            if line[:11] == "ITEM: ATOMS":
+                atom_line = line    # Obtains atom declaration line, as above
+                break
+            else:
+                continue
+        atom_columns = atom_line.split()
+        atom_columns = atom_columns[2:]   # Breaks the line into columns and removes the unnecessary first two columns
+        column_count = 0
+        for header in atom_columns:
+            if header == "x":             # Looks for the column containing the x-component of the atom position
+                break
+            else:
+                column_count += 1
+                continue
+        position_x_column = column_count
+        atom_positions = []           # Creates a list to store the position vector of the atom at each frame
+        line_count = 0
+        for line in raw_atom_lines:
+            position_vector = []    # Creates a list to store the vector for this frame
+            data_columns = line.split()
+            position_vector.append(data_columns[position_x_column])       # Stores the x-component into the position vector
+            position_vector.append(data_columns[position_x_column + 1])   # Stores the y-component into the position vector
+            position_vector.append(data_columns[position_x_column + 2])   # Stores the z-component into the position vector
+            atom_positions.append(position_vector)    # Adds the position vector into the list of position vectors
+        return atom_positions    # Returns the list of atom positions
+
+
+    def readAtomDipoles(lammpstrj, raw_atom_lines):
+        """ Function to extract the dipole vectors for each
+        atom, so that the positions of the dummy atoms can
+        be calculated.
+        lammpstrj - the raw, unedited trajectory.
+        raw_atom_lines - the raw, unedited lines, specific
+          one atom number. """
+        for line in lammpstrj:
+            if line[:11] == "ITEM: ATOMS":
+                atom_line = line    # Searches for the line that details the atom columns
+                break               # in order to identify the dipole columns
+            else:
+                continue
+        atom_columns = atom_line.split()  # Breaks up the column headings
+        atom_columns = atom_columns[2:]   # Removes the first two elements as they do not correspond to columns of data
+        column_count = 0                  # Used to keep track of the column numbers
+        for header in atom_columns:
+            if header == "mux":           # Looks for the column containing the x-component of the dipole
+                break
+            else:
+                column_count += 1
+                continue
+        dipole_x_column = column_count    # Sets the column number for the dipole x-component
+        atom_dipoles = []                 # Defines a list to contain the dipole vectors of the atom at each frame
+        for line in raw_atom_lines:
+            dipole_vector = []            # Defines a vector to hold each of the dipole components
+            data_columns = line.split()   # Breaks the data up into columns
+            dipole_vector.append(data_columns[dipole_x_column])       # Stores the x-component into the dipole_vector
+            dipole_vector.append(data_columns[dipole_x_column + 1])   # Stores the y-component into the dipole_vector
+            dipole_vector.append(data_columns[dipole_x_column + 2])   # Stores the z-component into the dipole_vector
+            atom_dipoles.append(dipole_vector)     # Adds the dipole vector onto the list for this atom
+        return atom_dipoles    # Returns the list of the vectors for this particular atom type
+
+    def filterRemove(raw_atom_lines, atom_type_remove):
+        """ Function to remove atoms of a certain atom type
+        (e.g. CG water) from the output trajectory.
+        raw_atom_lines - raw, unedited lines of all atoms.
+        atom_type_remove - atom type to be removed from the
+          trajectory. """
+        filtered_lines = []   # Defines a list to store the lines that pass through the filter
+        removed_IDs = []      # Stores the ID numbers of atoms that have been removed
+        remove_num = str(atom_type_remove)    # Converts the atom type to string format
+        for line in raw_atom_lines:
+            data_columns = line.split()
+            if data_columns[1] != remove_num:  # The line passes through the filter if the atom type
+                filtered_lines.append(line)    # does not match that which is to be removed
+                continue
+            else:
+                removed_IDs.append(data_columns[0])  # Adds the removed ID to the list
+                continue
+        return filtered_lines, removed_IDs   # Returns the set of lines containing the desired atom types
+
+    def extractFrame(self, number):
+        """
+        Function to extract a specific frame from a trajectory.
+        :param number: Frame number to be extracted
+        :return: Lines pertaining to the selected frame.
+        """
+        # Assuming that the number is counted from 1
+        count_line = 0
+        count_frame = 0
+        for line in self.all_lines:
+            count_line += 1
+            if line[:14] == "ITEM: TIMESTEP":
+                count_frame += 1
+                if count == number:
+                    break
+                continue
+            else:
+                continue
+        count = count_line
+        frame_lines = []
+        for line in range(count_line, len(self.all_lines):
+            if line[:11] == "ITEM: ATOMS":
+                for x in range(self.total_atoms):                        # Upon reaching the atom data section, the next (total_atoms)
+                    frame_lines.append(self.all_lines[count + x + 1])     # lines are extracted and stored into a list
+        return frame_lines
---
+++ process.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+
+
+import sys
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+from math import sqrt
+from optparse import OptionParser
+
+class Atom:
+    def __init__(self, atom_type, coords, dipole):
+        self.atom_type = atom_type
+        self.coords = coords
+        self.dipole = dipole
+
+    def __repr__(self):
+        return "<Atom {0} charge={1}, mass={2} @ {3}, {4}, {5}>".format(self.atom_type, *self.coords, *self.dipole)
+
+class Frame:
+    def __init__(self, num):
+        self.num = num
+        self.atoms = []
+
+    def __repr__(self):
+        return "<Frame {0} containing {1} atoms>\n{2}".format(self.num, len(self.atoms), self.title)
+        
+    def bond_length(self, num1, num2):
+        """
+        return Euclidean distance between two atoms
+        """
+        dist = np.linalg.norm(self.atoms[num1].loc - self.atoms[num2].loc)
+        #print("Distance between atoms {0} and {1} is {2}".format(num1, num2, dist))
+        return dist
+
+    def bond_length_atoms(self, atom1, atom2):
+        """
+        return Euclidean distance between two atoms
+        """
+        dist = sqrt((atom1.loc[0]-atom2.loc[0])**2 +  (atom1.loc[1]-atom2.loc[1])**2 + (atom1.loc[2]-atom2.loc[2])**2)
+        #dist = sqrt(sum((atom1.loc-atom2.loc)**2))
+        #dist = np.linalg.norm(atom1.loc - atom2.loc)
+        #print("Distance between atoms {0} and {1} is {2}".format(num1, num2, dist))
+        return dist
+    
+    def bond_angle(self, num1, num2, num3, num4):
+        """
+        angle at atom2 formed by bonds: 1-2 and 2-3
+        """
+        vec1 = (self.atoms[num2].loc - self.atoms[num1].loc)
+        vec2 = (self.atoms[num4].loc - self.atoms[num3].loc)
+        #vec1 = vec1 / np.linalg.norm(vec1)
+        vec1 = vec1 / sqrt(vec1[0]**2 + vec1[1]**2 + vec1[2]**2)
+        #vec2 = vec2 / np.linalg.norm(vec2)
+        vec2 = vec2 / sqrt(vec2[0]**2 + vec2[1]**2 + vec2[2]**2)
+        angle = np.arccos(np.dot(vec1, vec2))
+        return 180 - (angle * 180 / np.pi)
+    
+    def angle_norm_bisect(self, num1, num2, num3):
+        """
+        return normal vector to plane formed by 3 atoms and their bisecting vector
+        """ 
+        vec1 = (self.atoms[num2].loc - self.atoms[num1].loc)
+        vec2 = (self.atoms[num3].loc - self.atoms[num2].loc)
+        vec1 = vec1 / np.linalg.norm(vec1)
+        vec2 = vec2 / np.linalg.norm(vec2)
+        normal = np.cross(vec1, vec2)
+        bisec = (vec1 + vec2) / 2.
+        return polar_coords(normal), polar_coords(bisec)
+
+    def show_atoms(self, start=0, end=-1):
+        """
+        print coordinates of the atoms numbered start to end
+        """
+        print(self.title)
+        if end == -1:
+            end = len(self.atoms)
+        for i in xrange(start, end):
+            print(self.atoms[i])
+
+    def get_bond_lens(self, request=bond_pairs):
+        dists = np.zeros(len(request))
+        for i, pair in enumerate(request):
+            dists[i] = self.bond_length(self.atom_nums[pair[0]], self.atom_nums[pair[1]])
+        return dists
+
+    def get_bond_angles(self, request=bond_triples):
+        angles = np.zeros(len(request))
+        for i, triple in enumerate(request):
+            angles[i] = self.bond_angle(self.atom_nums[triple[0]], self.atom_nums[triple[1]], self.atom_nums[triple[1]], self.atom_nums[triple[2]])
+        return angles
+
+    def get_bond_dihedrals(self, request=bond_quads):
+        dihedrals = np.zeros(len(request))
+        for i, quad in enumerate(request):
+            dihedrals[i] = self.bond_angle(self.atom_nums[quad[0]], self.atom_nums[quad[1]], self.atom_nums[quad[2]], self.atom_nums[quad[3]])
+        return dihedrals
+
+def calc_measures(frames, req="length", request=bond_quads, export=True):
+    print("Calculating bond "+req+"s")
+    if export:
+        f = open("bond_"+req+"s.csv", "a")
+    t_start = time.clock()
+    measures = []
+    if export:
+        measure_names = ["-".join(name) for name in request]
+        f.write(",".join(measure_names) + "\n")
+    for i, frame in enumerate(frames):
+        perc = i * 100. / len(frames)
+        if(i%100 == 0):
+            sys.stdout.write("\r{:2.0f}% ".format(perc) + "X" * int(0.2*perc) + "-" * int(0.2*(100-perc)) )
+            sys.stdout.flush()
+        measures.append(frame.calc_measure[req](request))
+        if export:
+            measures_text = [str(num) for num in measures[-1]]
+            f.write(",".join(measures_text) + "\n")
+    avg = np.mean(measures, axis=0)
+    t_end = time.clock()
+    if export:
+        f.truncate()
+        f.close()
+    print("\rCalculated {0} frames in {1}s\n".format(len(frames), (t_end - t_start)) + "-"*20)
+    return measures, avg
+
+
+def polar_coords(xyz, axis1=np.array([0,0,0]), axis2=np.array([0,0,0]), mod=True):
+    """
+    Convert cartesian coordinates to polar, if axes are given it will be reoriented.
+    axis points to the north pole (latitude), axis2 points to 0 on equator (longitude)
+    if mod, do angles properly within -pi, +pi
+    """
+    tpi = 2*np.pi
+    polar = np.zeros(3)
+    xy = xyz[0]**2 + xyz[1]**2
+    polar[0] = np.sqrt(xy + xyz[2]**2)
+    polar[1] = np.arctan2(np.sqrt(xy), xyz[2]) - axis1[1]
+    polar[2] = np.arctan2(xyz[1], xyz[0]) - axis2[2]
+
+    if axis2[1]<0:
+        polar[2] = polar[2] + tpi
+    if mod:
+        polar[1] = polar[1]%(tpi)
+        polar[2] = polar[2]%(tpi)
+
+    return polar
+
+
+def print_output(output_all, output, request):
+    for name, val in zip(request, output):
+        print("{0}: {1:4.3f}".format("-".join(name), val))
+
+
+def graph_output(output_all):
+    rearrange = zip(*output_all)
+    plt.figure()
+
+    for i, item in enumerate(rearrange):
+        plt.subplot(2,3, i+1)
+        data = plt.hist(item, bins=100, normed=1)
+
+
+def analyse(filename, natoms):
+    """
+    Perform analysis of dipoles in LAMMPS trajectory
+    :param lammpstrj: LAMMPS trajectory
+    :return: Nothing
+    """
+    t_start = time.clock()
+    np.set_printoptions(precision=3, suppress=True)
+    frame = Frame()
+
+    if natoms == -1:
+        natoms = reader.getNumAtoms()
+    # Is it efficient to read the whole trajectory twice?
+    #nframes = reader.getNumFrames()
+    angles
+
+    # Read in frame from trajectory
+    with FrameReader(filename) as reader:
+        while reader.getFrame(frame):
+            # Process frame
+            angle1 = np.zeros(natoms)
+            angle2 = np.zeros(natoms)
+
+    t_end = time.clock()
+    print("\rCalculated {0} frames in {1}s\n".format(len(cg_frames), (t_end - t_start)) + "-"*20)
+    return len(cg_frames)
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-i", "--input",
+                      action="store", type="string", dest="lammpstrj", default="",
+                      help="Input file - LAMMPS trajectory", metavar="FILE")
+    parser.add_option("-n", "--natoms",
+                      action="store", type="int", dest="natoms", default="-1",
+                      help="Number of atoms to calculate for")
+    # parser.add_option("-v", "--verbose",
+    #                   action="store_true", dest="verbose", default=False,
+    #                   help="Make more verbose")
+    (options, args) = parser.parse_args()
+    verbose = options.verbose
+    if not options.lammpstrj:
+        print("Must provide LAMMPS trajectory to run")
+        sys.exit(1)
+    analyse(options.lammmpstrj, options.natoms)
